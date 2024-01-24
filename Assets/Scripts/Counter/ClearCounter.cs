d2e8c001bc125e2e1e7ddaeec2e7c037c793a54e
nicorm2000 <nicorm2000@gmail.com>
2024-01-24 17:28:59 -0300

    Basic moving ingredient system

--- Assets/Scripts/Counter/ClearCounter.cs
+++ Assets/Scripts/Counter/ClearCounter.cs
@@ -5,16 +5,57 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private Transform counterTopPoint;
     [SerializeField] private IngredientsSO ingredient;
 
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
     private IngredientObject ingredientObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (ingredientObject != null)
+            {
+                ingredientObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
         if (ingredientObject == null)
         {
             Transform ingredientTransform = Instantiate(ingredient.prefab, counterTopPoint);
-            ingredientTransform.localPosition = Vector3.zero;
-
-            ingredientObject = ingredientTransform.GetComponent<IngredientObject>();
+            ingredientTransform.GetComponent<IngredientObject>().SetClearCounter(this);
         }
+        else
+        {
+            Debug.Log(ingredientObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetIngredientObjectFollowTranform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetIngredientObject(IngredientObject ingredientObject)
+    {
+        this.ingredientObject = ingredientObject;
+    }
+
+    public IngredientObject GetIngredientObject()
+    {
+        return ingredientObject;
+    }
+
+    public void ClearIngredientObject()
+    {
+        ingredientObject = null;
+    }
+
+    public bool HasIngredientObject()
+    {
+        return ingredientObject != null;
     }
 }
--- Assets/Scripts/Ingredients/IngredientObject.cs
+++ Assets/Scripts/Ingredients/IngredientObject.cs
@@ -4,8 +4,35 @@ public class IngredientObject : MonoBehaviour
 {
     [SerializeField] private IngredientsSO ingredientSO;
 
+    private ClearCounter clearCounter;
+
     public IngredientsSO GetIngredientObjectSO()
     {
         return ingredientSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearIngredientObject();
+        }
+        
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasIngredientObject())
+        {
+            Debug.LogError("Counter already has an ingredient");
+        }
+
+        clearCounter.SetIngredientObject(this);
+
+        transform.parent = clearCounter.GetIngredientObjectFollowTranform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
