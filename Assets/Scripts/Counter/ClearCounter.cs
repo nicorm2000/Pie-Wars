eae3a3c180078b5ac9b1ec6363eb7fa55852c031
nicorm2000 <nicorm2000@gmail.com>
2024-01-24 16:38:17 -0300

    Fixed spawning bug

--- Assets/Scripts/Counter/ClearCounter.cs
+++ Assets/Scripts/Counter/ClearCounter.cs
@@ -5,10 +5,16 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private Transform counterTopPoint;
     [SerializeField] private IngredientsSO ingredient;
 
+    private IngredientObject ingredientObject;
+
     public void Interact()
     {
-        Debug.Log("Interact");
-        Transform ingredientTransform = Instantiate(ingredient.prefab, counterTopPoint);
-        ingredientTransform.localPosition = Vector3.zero;
+        if (ingredientObject == null)
+        {
+            Transform ingredientTransform = Instantiate(ingredient.prefab, counterTopPoint);
+            ingredientTransform.localPosition = Vector3.zero;
+
+            ingredientObject = ingredientTransform.GetComponent<IngredientObject>();
+        }
     }
 }
