6cff8e139987121ade7d0c02583c774335891375
nicorm2000 <nicorm2000@gmail.com>
2024-01-24 12:57:08 -0300

    Created counters that can be interacted with

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,8 +6,47 @@ public class Player : MonoBehaviour
     [SerializeField] private float rotateSpeed;
     [SerializeField] private float playerRadius;
     [SerializeField] private float playerHeight;
+    [SerializeField] private float interactDistance;
+    [SerializeField] private LayerMask countersLayerMask;
+
+    private Vector3 lastInteractDir;
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+            inputVector.y = 1;
+        if (Input.GetKey(KeyCode.S))
+            inputVector.y = -1;
+        if (Input.GetKey(KeyCode.A))
+            inputVector.x = -1;
+        if (Input.GetKey(KeyCode.D))
+            inputVector.x = 1;
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+            lastInteractDir = moveDir;
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -31,9 +70,9 @@ public class Player : MonoBehaviour
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f);
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-            if (canMove) 
-            { 
-                transform.position += moveDirX;
+            if (canMove)
+            {
+                moveDir = moveDirX;
             }
             else
             {
